47f25a1df7892bade75c08946cbeed3b28eb90bb
Muhaiminul-Islam <mikanon37@gmai.com>
2026-05-26 10:35:11 +0600

    subtraction part added in staging

--- src/calculator.js
+++ src/calculator.js
@@ -6,9 +6,16 @@ function add(a, b) {
   return a + b;
 }
 
+function subtract(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+  return a - b;
+}
 
 
 
 module.exports = {
-  add
+  add,
+  subtract
 };
