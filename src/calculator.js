566d6b093746c70d949116c57bc5369fdee2b5cf
Muhaiminul-Islam <mikanon37@gmai.com>
2026-05-26 09:29:19 +0600

    Calculator app initial Release:v1

---
+++ src/calculator.js
@@ -0,0 +1,14 @@
+// calculator.js
+function add(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both arguments must be numbers');
+  }
+  return a + b;
+}
+
+
+
+
+module.exports = {
+  add
+};
